4cd4868ef18f5dbc5729d113a920d6a786fbea0c
Haruka Hatori <hc.hatori@icloud.com>
2020-03-29 22:33:34 +0900

    leetcode #1342

---
+++ leetcode/1342/1342.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(numberOfSteps(7))
+}
+
+func numberOfSteps(num int) int {
+	steps := 0
+	for num > 0 {
+		if num%2 == 0 {
+			num /= 2
+		} else {
+			num--
+		}
+		steps++
+	}
+
+	return steps
+}

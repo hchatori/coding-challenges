e82c2e90af0e9d860677880b5079cc8ac6ceaacd
Haruka Hatori <hc.hatori@icloud.com>
2020-06-05 03:33:55 +0900

    project euler 18 + 67

---
+++ project-euler/67/67.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println(maxPathSum())
+}
+
+func maxPathSum() int {
+	triangle := [][]int{}
+
+	file, err := os.Open("project-euler/67/big-triangle.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		triangleRowString := strings.Fields(scanner.Text())
+		triangleRow := []int{}
+		for _, s := range triangleRowString {
+			n, _ := strconv.Atoi(s) // guaranteed s is a number, ignore err
+			triangleRow = append(triangleRow, n)
+		}
+		triangle = append(triangle, triangleRow)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for row := len(triangle) - 1; row > 0; row-- {
+		for col := 0; col < len(triangle[row])-1; col++ {
+			first := triangle[row][col]
+			second := triangle[row][col+1]
+			if first >= second {
+				triangle[row-1][col] += first
+			} else {
+				triangle[row-1][col] += second
+			}
+		}
+	}
+
+	return triangle[0][0]
+}

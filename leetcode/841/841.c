17c49dd691380343d2b6404b59b69aae4a6e0272
Haruka Hatori <hc.hatori@gmail.com>
2021-07-03 23:06:27 +0900

    clean up c code

--- leetcode/841/841.c
+++ leetcode/841/841.c
@@ -2,25 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_NUM_OF_ROOMS 1000
+
 bool canVisitAllRooms(int **rooms, int roomsSize, int *roomsColSize)
 {
 	// Create a roomsUnlocked array, initialized with 'false'.
-	bool *roomsUnlocked = malloc(roomsSize * sizeof(bool));
-	for (int i = 0; i < roomsSize; i++)
-	{
-		roomsUnlocked[i] = false;
-	}
-	roomsUnlocked[0] = true;
+	bool roomsUnlocked[MAX_NUM_OF_ROOMS] = {true};
 
 	// Create a counter that tracks the number of unlocked rooms. Start with 1
 	// because rooms[0] is always unlocked.
 	int numRoomsUnlocked = 1;
 
 	// Create 'queue', a block of memory of int pointers, initialized with 0.
-	int *queue = (int *)malloc(1000 * sizeof(int));
+	int queue[MAX_NUM_OF_ROOMS] = {0};
 	int front = 0;
 	int end = 0;
-	queue[end] = 0;
 	int numsInQueue = 1;
 
 	// For each number in the queue, for each key in rooms[i], check and update.
@@ -43,9 +39,6 @@ bool canVisitAllRooms(int **rooms, int roomsSize, int *roomsColSize)
 		numsInQueue--;
 	}
 
-	free(roomsUnlocked);
-	free(queue);
-
 	// Once the queue is empty, if numRoomsUnlocked == roomsSize, return true.
 	if (numRoomsUnlocked == roomsSize)
 	{
@@ -59,11 +52,7 @@ int main()
 {
 	int roomsSize = 4;
 
-	int *roomsColSize = (int *)malloc(roomsSize * sizeof(int));
-	roomsColSize[0] = 2;
-	roomsColSize[1] = 3;
-	roomsColSize[2] = 1;
-	roomsColSize[3] = 1;
+	int roomsColSize[] = {2, 3, 1, 1};
 
 	// First, allocate memory for rooms, then allocate memory for each room in
 	// rooms.
